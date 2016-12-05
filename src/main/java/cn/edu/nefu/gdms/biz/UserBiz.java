df554cebc2d1755f2729cccc9f3c0445ab9bf1af
dingyunxiang <1210955086@qq.com>
2016-12-05 23:20:55 +0800

    login相关接口

---
+++ src/main/java/cn/edu/nefu/gdms/biz/UserBiz.java
@@ -0,0 +1,7 @@
+package cn.edu.nefu.gdms.biz;
+
+/**
+ * Created by dingyunxiang on 16/12/5.
+ */
+public class UserBiz {
+}
---
+++ src/main/java/cn/edu/nefu/gdms/web/UserController.java
@@ -0,0 +1,7 @@
+package cn.edu.nefu.gdms.web;
+
+/**
+ * Created by dingyunxiang on 16/12/5.
+ */
+public class LoginController {
+}
