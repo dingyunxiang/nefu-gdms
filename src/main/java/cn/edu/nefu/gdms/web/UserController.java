cef15b8045ae97cfe73295866cc9e5b5bc813acd
dingyunxiang <1210955086@qq.com>
2016-12-12 10:47:50 +0800

    修复spring路径问题

--- src/main/java/cn/edu/nefu/gdms/web/UserController.java
+++ src/main/java/cn/edu/nefu/gdms/web/UserController.java
@@ -5,22 +5,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by dingyunxiang on 16/12/5.
  */
 @Controller
-@RequestMapping("userInfo")
+@RequestMapping("/userInfo")
 public class UserController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private UserBiz userBiz;
 
-    @RequestMapping(value = "login" ,method = RequestMethod.GET)
+    @RequestMapping(value = "/login" ,method = RequestMethod.GET)
+    @ResponseBody
     public String login(@RequestParam("username") String username,
                         @RequestParam("password") String password){
         return userBiz.login(username,password)+"";
